722b887f1962db144b574a7a1d299e58b86163f2
Mcsvip <felicitycfgreene@gmail.com>
2024-07-21 19:13:46 +0800

    my personal configuration update

--- wezterm/config/font.lua
+++ wezterm/config/font.lua
@@ -1,9 +1,7 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-  font = wezterm.font 'JetBrains Mono',
-  font_size = 14,
-  freetype_load_flags = "NO_HINTING", -- smoother font
+	font = wezterm.font("JetBrains Mono"),
+	font_size = 16,
+	freetype_load_flags = "NO_HINTING", -- smoother font
 }
-
-
